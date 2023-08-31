98b120e669e2070c84f01df975ac4ee57fa0ba97
HirenGohil1212 <21amtics017@gmail.com>
2023-08-31 18:56:04 +0530

    Initial Commit

--- app/src/main/java/com/example/photoeditorpro/Create_Panel.java
+++ app/src/main/java/com/example/photoeditorpro/Create_Panel.java
@@ -1,14 +1,81 @@
 package com.example.photoeditorpro;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.nio.file.Path;
 
 public class Create_Panel extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_panel);
+
+        imageView = findViewById(R.id.my_image);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                pickImageActivityResultLauncher.launch(intent);
+
+            }
+        });
+
+
     }
+
+    public String getPathFromURI(Uri contentUri){
+        String res = null;
+
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri,proj,null,null,null);
+        if(cursor.moveToFirst()){
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+
+        }
+cursor.close();
+        return res;
+
+    }
+    ActivityResultLauncher<Intent> pickImageActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri selectedImageUri = data.getData();
+
+                        final String path = getPathFromURI(selectedImageUri);
+
+                        if(path!= null){
+                            File f = new File(path);
+                            selectedImageUri = Uri.fromFile(f);
+                        }
+
+                        imageView.setImageURI(selectedImageUri);
+
+                    }
+                }
+            });
+
 }
--- app/src/main/java/com/example/photoeditorpro/Edit_Panel.java
+++ app/src/main/java/com/example/photoeditorpro/Edit_Panel.java
@@ -1,14 +1,32 @@
 package com.example.photoeditorpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Edit_Panel extends AppCompatActivity {
+
+   CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_panel);
+
+        cardView = findViewById(R.id.panel_view);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Edit_Panel.this, Create_Panel.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
